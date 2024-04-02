684f2a3822f4001af3ce11677e92d8c21faf8871
daryl.wei <18706019630@163.com>
2024-04-02 11:44:13 +0800

    3. 引入日志框架logback

--- oceanflow-web/src/main/java/com/wei/oceanflow/web/TestController.java
+++ oceanflow-web/src/main/java/com/wei/oceanflow/web/TestController.java
@@ -1,5 +1,6 @@
 package com.wei.oceanflow.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class TestController {
+
 
     @RequestMapping("/test")
     private String test() {
-        return "HelloWorld";
+        System.out.println("sout:helloworld");
+        log.info("log:helloworld");
+        return "helloworld";
     }
 }
